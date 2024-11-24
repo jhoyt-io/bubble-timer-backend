aca3528eba3f4e8fef5c30d03051df77bf3603d0
Jordan Hoyt <admin@jhoyt.io>
2024-11-24 14:17:16 -0800

    Add back the lambda results to satisfy the integration

--- lib/bubble-timer-backend-stack.websocket.ts
+++ lib/bubble-timer-backend-stack.websocket.ts
@@ -3,4 +3,18 @@
 export async function handler(event: any, context: any) {
     console.log("Event: " + JSON.stringify(event));
     console.log("Context: " + JSON.stringify(context));
+
+    const resultBody: string = "{}";
+
+    return {
+        "isBase64Encoded": false,
+        "statusCode": 200,
+        "headers": { 
+            "Access-Control-Allow-Headers" :  "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+            "Access-Control-Allow-Origin": "http://localhost:4000",
+            "Access-Control-Allow-Methods": "OPTIONS,GET,POST,PUT,PATCH,DELETE",
+            "Content-Type": "application/json",
+        },
+        "body": resultBody,
+    }
 }
