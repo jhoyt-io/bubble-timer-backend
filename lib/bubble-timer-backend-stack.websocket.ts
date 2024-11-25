37f253dcf19553ee9d09c8e4ae54bdeadd81c1c1
Jordan Hoyt <admin@jhoyt.io>
2024-11-24 21:45:48 -0800

    Validate auth token

--- lib/bubble-timer-backend-stack.websocket.ts
+++ lib/bubble-timer-backend-stack.websocket.ts
@@ -1,10 +1,29 @@
 // import { getTimer, updateTimer, Timer } from "./backend/timers";
+import { CognitoJwtVerifier } from 'aws-jwt-verify';
+
+const jwtVerifier = CognitoJwtVerifier.create({
+    userPoolId: 'us-east-1_cjED6eOHp',
+    tokenUse: 'id',
+    clientId: '4t3c5p3875qboh3p1va2t9q63c',
+})
 
 export async function handler(event: any, context: any) {
     console.log("Event: " + JSON.stringify(event));
     console.log("Context: " + JSON.stringify(context));
 
+    //     "body": "{\"action\":\"sendmessage\",\"data\":{\"Hello\":\"WORLD?????\"}}",
+    event.body
+
+    const connectionId = event.requestContext.connectionId;
+    const cognitoToken = event.headers.Authorization;
+
     const resultBody: string = "{}";
+    try {
+        const payload = await jwtVerifier.verify(cognitoToken);
+        console.log("Token is valid. Payload: ", payload);
+    } catch {
+        console.log("Token not valid!");
+    }
 
     return {
         "isBase64Encoded": false,
