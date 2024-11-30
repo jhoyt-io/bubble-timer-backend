ba885f8ce24bae551340caf57279ca57bc73c94e
Jordan Hoyt <admin@jhoyt.io>
2024-11-29 21:32:04 -0800

    Don't try and use a buffer

--- lib/bubble-timer-backend-stack.websocket.ts
+++ lib/bubble-timer-backend-stack.websocket.ts
@@ -76,7 +76,7 @@ export async function handler(event: any, context: any) {
                     // Don't send back to self...
                     if (connection.deviceId !== deviceId) {
                         const command = new PostToConnectionCommand({
-                            Data: Buffer.from(data),
+                            Data: data,
                             ConnectionId: connection.connectionId,
                         });
 
